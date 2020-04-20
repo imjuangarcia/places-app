f1580ab4c8d1f87121ca04b6c0cd79468bb97cfe
Juan Martín García <hello@juangarcia.com.ar>
2020-04-20 10:55:48 -0300

    Basic screen setup

---
+++ screens/PlacesListScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const PlacesListScreen = (props) => {
+  const test = true;
+
+  return (
+    <View>
+      <Text>PlacesListScreen</Text>
+    </View>
+  );
+};
+
+PlacesListScreen.navigationOptions = {
+  headerTitle: 'All Places',
+};
+
+const styles = StyleSheet.create({});
+
+export default PlacesListScreen;
